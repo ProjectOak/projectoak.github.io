ff199ba5fcb3e2ac057189364a23d417f1de8fcb
ProjectOak <thiagoparizi@gmail.com>
2020-03-18 08:30:37 +0100

    Many changes

--- js/interact.js
+++ js/interact.js
@@ -1,11 +1,37 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// HOME PAGE HERO ANIMATION
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+anime({
+  targets: '.el',
+  keyframes: [
+    {width: ['0%', '100%']}
+  ],
+  easing: 'easeInOutQuad',
+  duration: 500,
+  delay: 500
+});
+
+anime({
+  targets: '.el-header',
+  opacity: ['0%', '100%'],
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  delay: 1000
+});
+
+// ARTICLE ANIMATIONS
+
+anime({
+  targets: '.half',
+  opacity: ['0%', '100%'],
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  delay: 200
+});
+
+anime({
+  targets: '.small-grid',
+  opacity: ['0%', '100%'],
+  easing: 'easeInOutQuad',
+  duration: 1000,
+  delay: 1200
 });
