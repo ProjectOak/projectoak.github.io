a1b4a6b436bdf05a5a3e26459c98d4e618f02a1a
ProjectOak <thiagoparizi@gmail.com>
2020-03-20 01:42:48 +0100

    Animation changes and content

--- js/interact.js
+++ js/interact.js
@@ -1,20 +1,30 @@
 // HOME PAGE HERO ANIMATION
 
 anime({
-  targets: '.el',
-  height: ['0%', '100%'],
-  easing: 'easeInOutQuad',
-  duration: 500,
-  delay: 500
-});
-
-anime({
   targets: '.el-header',
   opacity: ['0%', '100%'],
   scale: [0.85, 1],
   easing: 'easeInOutQuad',
   duration: 300,
-  delay: 1000
+  delay: 500
+});
+
+anime({
+  targets: '.wave',
+  rotate: [0, 45],
+  easing: 'easeInOutSine',
+  duration: 1000,
+  direction: 'alternate',
+  loop: true,
+});
+
+anime({
+  targets: '.scroll-down',
+  opacity: ['0%', '100%'],
+  translateY: 16,
+  easing: 'easeInOutQuad',
+  duration: 500,
+  delay: 1200
 });
 
 // ARTICLE ANIMATIONS
