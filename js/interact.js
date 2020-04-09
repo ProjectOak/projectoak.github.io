b3e20350b4492ad7e6e44c04a5412344362803e0
ProjectOak <thiagoparizi@gmail.com>
2020-04-09 10:10:40 +0200

    Radical refactoring, too much to go into detail

--- js/interact.js
+++ js/interact.js
@@ -1,46 +1,120 @@
-// HOME PAGE HERO ANIMATION
+document.addEventListener('DOMContentLoaded', () =>{
+  anime({
+    targets: '.animate-header',
+    opacity: ['0', '1'],
+    scale: [0.85, 1],
+    easing: 'easeInOutQuad',
+    duration: 300,
+    delay: 500
+  });
 
-anime({
-  targets: '.el-header',
-  opacity: ['0%', '100%'],
-  scale: [0.85, 1],
-  easing: 'easeInOutQuad',
-  duration: 300,
-  delay: 500
-});
+  anime({
+    targets: '.feather-chevron-down',
+    opacity: ['0', '1'],
+    translateY: 16,
+    easing: 'easeInOutQuad',
+    duration: 500,
+    delay: 1000
+  });
 
-anime({
-  targets: '.wave',
-  rotate: [0, 45],
-  easing: 'easeInOutSine',
-  duration: 1000,
-  direction: 'alternate',
-  loop: true,
-});
+  // ARTICLE ANIMATIONS
 
-anime({
-  targets: '.scroll-down',
-  opacity: ['0%', '100%'],
-  translateY: 16,
-  easing: 'easeInOutQuad',
-  duration: 500,
-  delay: 1200
-});
+  anime({
+    targets: '.opacity',
+    opacity: ['0', '1'],
+    easing: 'easeInOutQuad',
+    duration: 500,
+    delay: 500
+  });
 
-// ARTICLE ANIMATIONS
+  // WAYPOINTS
 
-anime({
-  targets: '.half',
-  opacity: ['0%', '100%'],
-  easing: 'easeInOutQuad',
-  duration: 500,
-  delay: 200
-});
+  var waypointOne = new Waypoint({
+    element: document.getElementById('article-01'),
+    handler: function(direction){
+      if(direction == 'down'){
+        articleOne.play();
+      }
+      this.destroy()
+    },
+    offset: '50%'
+  })
 
-anime({
-  targets: '.small-grid',
-  opacity: ['0%', '100%'],
-  easing: 'easeInOutQuad',
-  duration: 500,
-  delay: 700
-});
+  var waypointTwo = new Waypoint({
+    element: document.getElementById('article-02'),
+    handler: function(direction){
+      if(direction == 'down'){
+        articleTwo.play();
+      }
+      this.destroy()
+    },
+    offset: '50%'
+  })
+
+  var waypointThree = new Waypoint({
+    element: document.getElementById('article-03'),
+    handler: function(direction){
+      if(direction == 'down'){
+        articleThree.play();
+      }
+      this.destroy()
+    },
+    offset: '50%'
+  })
+
+  var waypointFour = new Waypoint({
+    element: document.getElementById('article-04'),
+    handler: function(direction){
+      if(direction == 'down'){
+        articleFour.play();
+      }
+      this.destroy()
+    },
+    offset: '50%'
+  })
+
+  var articleOne = anime({
+    targets: '.post-url-01',
+    opacity: ['0', '1'],
+    translateX: ['250px', '0px'],
+    easing: 'easeInOutQuad',
+    duration: 500,
+    autoplay: false
+  });
+
+  var articleTwo = anime({
+    targets: '.post-url-02',
+    opacity: ['0', '1'],
+    translateX: ['-250px', '0px'],
+    easing: 'easeInOutQuad',
+    duration: 500,
+    autoplay: false
+  });
+
+  var articleThree = anime({
+    targets: '.post-url-03',
+    opacity: ['0', '1'],
+    translateX: ['250px', '0px'],
+    easing: 'easeInOutQuad',
+    duration: 500,
+    autoplay: false
+  });
+
+  var articleFour = anime({
+    targets: '.post-url-04',
+    opacity: ['0', '1'],
+    easing: 'easeInOutQuad',
+    duration: 500,
+    autoplay: false
+  });
+})
+
+function createTimedLink(element, callback, timeout){
+  setTimeout( function(){callback(element);}, timeout);
+  return false;
+}
+
+function myFunction(element) { 
+/* Block of code, with no 'return false'. */
+  window.location = element.href;
+ }
